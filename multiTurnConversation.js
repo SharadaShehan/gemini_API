a8e3b34e7be3cdd0be919f5a78abe83efdb39b78
Sharada Shehan <sharadashehan6@gmail.com>
2024-01-22 12:49:16 +0530

    muti turn conversation

--- multiTurnConversation.js
+++ multiTurnConversation.js
@@ -20,34 +20,6 @@ function promptAsync(question) {
 }
 
 // get complete response
-// async function run() {
-//     while (true) {
-//         const chat = model.startChat({
-//             history,
-//             generationConfig: {
-//             maxOutputTokens: 100,
-//             },
-//         });
-//         const userText = await promptAsync(`User: `);
-//         const result = await chat.sendMessage(userText);
-//         const responseText = (await result.response).text();
-//         console.log(`Bot: ${responseText}`);
-//         history.push(
-//             {
-//                 role: "user",
-//                 parts: userText
-//             }
-//         )
-//         history.push(
-//             {
-//                 role: "model",
-//                 parts: responseText
-//             }
-//         )
-//     }
-// }
-
-// get response in chunks
 async function run() {
     while (true) {
         const chat = model.startChat({
@@ -57,14 +29,9 @@ async function run() {
             },
         });
         const userText = await promptAsync(`User: `);
-        const result = await model.generateContentStream(userText);
-        let responseText = '';
-        console.log("Bot: ")
-        for await (const chunk of result.stream) {
-            const chunkText = chunk.text();
-            console.log(chunkText);
-            responseText += chunkText;
-        }
+        const result = await chat.sendMessage(userText);
+        const responseText = (await result.response).text();
+        console.log(`Bot: ${responseText}`);
         history.push(
             {
                 role: "user",
@@ -79,5 +46,38 @@ async function run() {
         )
     }
 }
+
+// // get response in chunks
+// async function run() {
+//     while (true) {
+//         const chat = model.startChat({
+//             history,
+//             generationConfig: {
+//             maxOutputTokens: 100,
+//             },
+//         });
+//         const userText = await promptAsync(`User: `);
+//         const result = await model.generateContentStream(userText);
+//         let responseText = '';
+//         console.log("Bot: ")
+//         for await (const chunk of result.stream) {
+//             const chunkText = chunk.text();
+//             console.log(chunkText);
+//             responseText += chunkText;
+//         }
+//         history.push(
+//             {
+//                 role: "user",
+//                 parts: userText
+//             }
+//         )
+//         history.push(
+//             {
+//                 role: "model",
+//                 parts: responseText
+//             }
+//         )
+//     }
+// }
 
 run();
